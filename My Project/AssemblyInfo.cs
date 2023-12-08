d57e6c94ccad6b5a0229ef25fe97b2cc7d87b0cb
hchsuppoort <149012260+hchsuppoort@users.noreply.github.com>
2023-12-08 15:12:49 -0700

    Add files via upload

---
+++ My Project/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("BossAdmin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Michele Brown, Consulting")]
+[assembly: AssemblyProduct("BossAdmin")]
+[assembly: AssemblyCopyright("Copyright © Michele Brown, Consulting 2012")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dd00464-c475-49ad-9228-67eb9ac531ba")]
+
+// Version information for an assembly consists of the following four values:
+// 
+// Major Version
+// Minor Version 
+// Build Number
+// Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")> 
+
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
---
+++ My Project/MyNamespace.Dynamic.Designer.cs
@@ -0,0 +1,567 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace BossAdmin.My
+{
+    internal static partial class MyProject
+    {
+        internal partial class MyForms
+        {
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public CostAnalysisPlanChooser m_CostAnalysisPlanChooser;
+
+            public CostAnalysisPlanChooser CostAnalysisPlanChooser
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_CostAnalysisPlanChooser=Create__Instance__(m_CostAnalysisPlanChooser);
+                    return m_CostAnalysisPlanChooser;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_CostAnalysisPlanChooser))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_CostAnalysisPlanChooser);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public CostAnalysisSearch m_CostAnalysisSearch;
+
+            public CostAnalysisSearch CostAnalysisSearch
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_CostAnalysisSearch=Create__Instance__(m_CostAnalysisSearch);
+                    return m_CostAnalysisSearch;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_CostAnalysisSearch))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_CostAnalysisSearch);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public Divisions m_Divisions;
+
+            public Divisions Divisions
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_Divisions=Create__Instance__(m_Divisions);
+                    return m_Divisions;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_Divisions))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_Divisions);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public ErrorDisplay m_ErrorDisplay;
+
+            public ErrorDisplay ErrorDisplay
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_ErrorDisplay=Create__Instance__(m_ErrorDisplay);
+                    return m_ErrorDisplay;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_ErrorDisplay))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_ErrorDisplay);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public ErrorLog m_ErrorLog;
+
+            public ErrorLog ErrorLog
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_ErrorLog=Create__Instance__(m_ErrorLog);
+                    return m_ErrorLog;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_ErrorLog))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_ErrorLog);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public EstimateView m_EstimateView;
+
+            public EstimateView EstimateView
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_EstimateView=Create__Instance__(m_EstimateView);
+                    return m_EstimateView;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_EstimateView))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_EstimateView);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public Explorer1 m_Explorer1;
+
+            public Explorer1 Explorer1
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_Explorer1=Create__Instance__(m_Explorer1);
+                    return m_Explorer1;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_Explorer1))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_Explorer1);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public Form1 m_Form1;
+
+            public Form1 Form1
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_Form1=Create__Instance__(m_Form1);
+                    return m_Form1;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_Form1))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_Form1);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public Login m_Login;
+
+            public Login Login
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_Login=Create__Instance__(m_Login);
+                    return m_Login;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_Login))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_Login);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public mdiMain m_mdiMain;
+
+            public mdiMain mdiMain
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_mdiMain=Create__Instance__(m_mdiMain);
+                    return m_mdiMain;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_mdiMain))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_mdiMain);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public Milestones m_Milestones;
+
+            public Milestones Milestones
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_Milestones=Create__Instance__(m_Milestones);
+                    return m_Milestones;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_Milestones))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_Milestones);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public PDFViewer m_PDFViewer;
+
+            public PDFViewer PDFViewer
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_PDFViewer=Create__Instance__(m_PDFViewer);
+                    return m_PDFViewer;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_PDFViewer))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_PDFViewer);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public PlanEstimateAddRows m_PlanEstimateAddRows;
+
+            public PlanEstimateAddRows PlanEstimateAddRows
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_PlanEstimateAddRows=Create__Instance__(m_PlanEstimateAddRows);
+                    return m_PlanEstimateAddRows;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_PlanEstimateAddRows))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_PlanEstimateAddRows);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public PlanEstimateTablesMaintenance m_PlanEstimateTablesMaintenance;
+
+            public PlanEstimateTablesMaintenance PlanEstimateTablesMaintenance
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_PlanEstimateTablesMaintenance=Create__Instance__(m_PlanEstimateTablesMaintenance);
+                    return m_PlanEstimateTablesMaintenance;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_PlanEstimateTablesMaintenance))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_PlanEstimateTablesMaintenance);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public PlanRoomsAdd m_PlanRoomsAdd;
+
+            public PlanRoomsAdd PlanRoomsAdd
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_PlanRoomsAdd=Create__Instance__(m_PlanRoomsAdd);
+                    return m_PlanRoomsAdd;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_PlanRoomsAdd))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_PlanRoomsAdd);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public PlanRoomsMaintenance m_PlanRoomsMaintenance;
+
+            public PlanRoomsMaintenance PlanRoomsMaintenance
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_PlanRoomsMaintenance=Create__Instance__(m_PlanRoomsMaintenance);
+                    return m_PlanRoomsMaintenance;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_PlanRoomsMaintenance))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_PlanRoomsMaintenance);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public Positions m_Positions;
+
+            public Positions Positions
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_Positions=Create__Instance__(m_Positions);
+                    return m_Positions;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_Positions))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_Positions);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public ProjectMaintenance m_ProjectMaintenance;
+
+            public ProjectMaintenance ProjectMaintenance
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_ProjectMaintenance=Create__Instance__(m_ProjectMaintenance);
+                    return m_ProjectMaintenance;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_ProjectMaintenance))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_ProjectMaintenance);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public ReportViewer m_ReportViewer;
+
+            public ReportViewer ReportViewer
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_ReportViewer=Create__Instance__(m_ReportViewer);
+                    return m_ReportViewer;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_ReportViewer))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_ReportViewer);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public SplashScreen1 m_SplashScreen1;
+
+            public SplashScreen1 SplashScreen1
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_SplashScreen1=Create__Instance__(m_SplashScreen1);
+                    return m_SplashScreen1;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_SplashScreen1))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_SplashScreen1);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public UserMaintenance m_UserMaintenance;
+
+            public UserMaintenance UserMaintenance
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_UserMaintenance=Create__Instance__(m_UserMaintenance);
+                    return m_UserMaintenance;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_UserMaintenance))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_UserMaintenance);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public UserSelect m_UserSelect;
+
+            public UserSelect UserSelect
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_UserSelect=Create__Instance__(m_UserSelect);
+                    return m_UserSelect;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_UserSelect))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_UserSelect);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public VendorCosts m_VendorCosts;
+
+            public VendorCosts VendorCosts
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_VendorCosts=Create__Instance__(m_VendorCosts);
+                    return m_VendorCosts;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_VendorCosts))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_VendorCosts);
+                }
+            }
+
+
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public VPOReasons m_VPOReasons;
+
+            public VPOReasons VPOReasons
+            {
+                [DebuggerHidden]
+                get
+                {
+                    m_VPOReasons=Create__Instance__(m_VPOReasons);
+                    return m_VPOReasons;
+                }
+                [DebuggerHidden]
+                set
+                {
+                    if (ReferenceEquals(value, m_VPOReasons))
+                        return;
+                    if (value is not null)
+                        throw new ArgumentException("Property can only be set to Nothing");
+                    Dispose__Instance__(ref m_VPOReasons);
+                }
+            }
+
+        }
+
+
+    }
+}
